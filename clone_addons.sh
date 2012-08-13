160749a99a5f589e6f662f45745045b0754f4702
obviousjim <james@jamesgeorge.org>
2012-08-13 12:33:47 +0900

    clone add ons script added

---
+++ clone_addons.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Dependencies for Duration
+
+cd ../../addons/
+echo "one is " $1
+if [ -z $1 ]; then
+    PREFIX="git clone https://github.com/"
+else
+    PREFIX="git clone git@github.com:"
+fi
+
+${PREFIX}YCAMInterlab/ofxTimecode.git
+
+${PREFIX}obviousjim/ofxTween.git
+
+${PREFIX}obviousjim/ofxUI.git
+cd ofxUI/
+git checkout develop
+cd ..
+
+${PREFIX}obviousjim/ofxMSATimer.git
+
+${PREFIX}Flightphase/ofxTextInputField.git
+
+${PREFIX}Flightphase/ofxRange.git
+
+${PREFIX}YCAMInterlab/ofxTimeline.git
+cd ofxTimeline/
+git checkout develop
+cd ..
+
