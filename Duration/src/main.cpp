b5be2a8332e6ed6fe01619c945d75bd7a7933c2a
Pierre Proske <pierre@digitalstar.net>
2014-04-18 11:54:55 +1000

    updated to original resolution

--- Duration/src/main.cpp
+++ Duration/src/main.cpp
@@ -4,12 +4,12 @@
 //========================================================================
 int main( ){
 
-	ofSetupOpenGL(1024,768, OF_WINDOW);			// <-------- setup the GL context
+	ofSetupOpenGL(1300,768, OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
-	ofSetWindowTitle("Duration");    
+	ofSetWindowTitle("Duration");
 	ofRunApp( new ofApp());
 
 }
